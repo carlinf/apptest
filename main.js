6925610abd96223d0e71d3ff2d984c519f06362b
carlinf <carlinfernandes077@gmail.com>
2022-08-13 12:16:26 +0530

    First commit

---
+++ main.js
@@ -0,0 +1,36 @@
+
+require('dotenv').config()
+const express =  require('express')
+const app = express()
+
+const { MongoClient } = require('mongodb');
+
+const url = process.env.MONGO_URL;
+const client = new MongoClient(url);
+const dbName = 'books';
+
+const db = client.db(dbName);
+const collection = db.collection('books');
+
+
+
+app.use("/static", express.static("static"))
+
+app.get("/books", async function(req,res){
+
+    // let list = [
+    //     {title: "Harry Potter", author: "Jk R", genre: "Friction"},
+    //     {title: "Dummy Book" ,author: "Jk C", genre: "Friction"},
+    //     {title: "Let us C", author: "Yashwant K", genre: "Education"},
+
+
+    // ]
+
+    let list = await collection.find({}).toArray();
+
+    res.json(list)
+
+})
+
+app.listen(3000)
+
